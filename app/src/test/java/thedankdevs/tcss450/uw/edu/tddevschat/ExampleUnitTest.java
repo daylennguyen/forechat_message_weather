fe9201021cdee1c082024652dc2c9aec9ec54a2a
Bryan <paolo186@gmail.com>
2018-10-26 12:24:52 -0700

    First Android Project Commit

---
+++ app/src/test/java/thedankdevs/tcss450/uw/edu/tddevschat/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package thedankdevs.tcss450.uw.edu.tddevschat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
