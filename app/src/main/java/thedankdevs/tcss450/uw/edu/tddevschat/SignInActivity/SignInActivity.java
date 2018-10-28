0406f5559a2f9486cf5727a52ba361cc77b1be70
hotgarbage360 <38119597+hotgarbage360@users.noreply.github.com>
2018-10-28 15:21:33 -0700

    added register fragment in register fragment!

--- app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/SignInActivity/SignInActivity.java
+++ app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/SignInActivity/SignInActivity.java
@@ -3,6 +3,9 @@ package thedankdevs.tcss450.uw.edu.tddevschat.SignInActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import thedankdevs.tcss450.uw.edu.tddevschat.R;
+
+
 public class SignInActivity extends AppCompatActivity {
 
     @Override
