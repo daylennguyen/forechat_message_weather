059de7de6a0ddd2486f112a0854869ea5fc66260
Michelle Brown <mcb35@uw.edu>
2018-11-05 00:31:04 -0800

    UI work

--- app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/HomeActivity/HomeActivity.java
+++ app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/HomeActivity/HomeActivity.java
@@ -82,19 +82,19 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_connections) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_weather) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        } //else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
