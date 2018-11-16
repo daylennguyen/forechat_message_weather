a9452569bb42cb056132b9fcf95dae0f5bc12800
Michelle Brown <mcb35@uw.edu>
2018-11-15 19:46:06 -0800

    Added a recyclerview and other classes for group chats.

---
+++ app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/HomeActivity/Chats/MyChatsRecyclerViewAdapter.java
@@ -0,0 +1,71 @@
+package thedankdevs.tcss450.uw.edu.tddevschat.HomeActivity.Chats;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import thedankdevs.tcss450.uw.edu.tddevschat.HomeActivity.Chats.ChatsFragment.OnListFragmentInteractionListener;
+import thedankdevs.tcss450.uw.edu.tddevschat.HomeActivity.Chats.content.Chat;
+
+import java.util.List;
+
+import thedankdevs.tcss450.uw.edu.tddevschat.R;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link Chat} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyChatsRecyclerViewAdapter extends RecyclerView.Adapter<MyChatsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Chat> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyChatsRecyclerViewAdapter(List<Chat> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_chats, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mName.setText(mValues.get(position).getName());
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mName;
+        public Chat mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mName = (TextView) view.findViewById(R.id.tv_chatslist_name);
+        }
+    }
+}
