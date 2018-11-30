af0f6130c7109a6b2cc910087f35649ff40ced4e
daylennguyen <daylen3@uw.edu>
2018-11-30 05:06:01 -0800

    Style changes added, code cleanup. Nearly Completed with weather settings; need to make small changes to map selection after sleeping.
    Signed-off-by: daylennguyen <daylen3@uw.edu>

--- app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/SignInActivity/SignInActivity.java
+++ app/src/main/java/thedankdevs/tcss450/uw/edu/tddevschat/SignInActivity/SignInActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
-
 import thedankdevs.tcss450.uw.edu.tddevschat.HomeActivity.HomeActivity;
 import thedankdevs.tcss450.uw.edu.tddevschat.R;
 import thedankdevs.tcss450.uw.edu.tddevschat.WaitFragment;
@@ -24,32 +23,34 @@ public class SignInActivity extends AppCompatActivity
         RegisterFragment.OnRegisterFragmentInteractionListener,
         VerifyFragment.OnVerifyFragmentInteractionListener {
 
-    /** Information from notification **/
-    private String mChatIDfromNotification;
-    private boolean mLoadFromChatNotification = false;
-    private static final String TAG = SignInActivity.class.getSimpleName();
+    private static final String  TAG                       = SignInActivity.class.getSimpleName();
+    /**
+     * Information from notification
+     **/
+    private              String  mChatIDfromNotification;
+    private              boolean mLoadFromChatNotification = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-        if (getIntent().getExtras() != null) {
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_sign_in );
+        if ( getIntent().getExtras() != null ) {
             //If there is message notification, grab all of the information.
-            if (getIntent().getExtras().containsKey("type")) {
-                Log.d(TAG, "type of message: " + getIntent().getExtras().getString("type"));
-                Log.d(TAG, "chatID from notification: " + getIntent().getExtras().getString("chatID"));
+            if ( getIntent().getExtras().containsKey( "type" ) ) {
+                Log.d( TAG, "type of message: " + getIntent().getExtras().getString( "type" ) );
+                Log.d( TAG, "chatID from notification: " + getIntent().getExtras().getString( "chatID" ) );
 
                 //Grab all of message information.
-                mLoadFromChatNotification = getIntent().getExtras().getString("type").equals("contact");
-                mChatIDfromNotification = getIntent().getExtras().getString("chatID");
+                mLoadFromChatNotification = getIntent().getExtras().getString( "type" ).equals( "contact" );
+                mChatIDfromNotification = getIntent().getExtras().getString( "chatID" );
             } else {
-                Log.d(TAG, "NO MESSAGE");
+                Log.d( TAG, "NO MESSAGE" );
             }
         }
-        if(savedInstanceState == null) {
-            if (findViewById(R.id.frame_signin_container) != null) {
+        if ( savedInstanceState == null ) {
+            if ( findViewById( R.id.frame_signin_container ) != null ) {
                 getSupportFragmentManager().beginTransaction()
-                        .add(R.id.frame_signin_container, new LoginFragment())
+                        .add( R.id.frame_signin_container, new LoginFragment() )
                         .commit();
             }
         }
@@ -62,39 +63,40 @@ public class SignInActivity extends AppCompatActivity
      * @param credentials
      */
     @Override
-    public void onLoginSuccess(Credentials credentials) {
-        openMain(credentials);
+    public void onLoginSuccess( Credentials credentials ) {
+        openMain( credentials );
     }
 
 
     /**
      * This method is called after verification email has sent again to the user.
      * Send the user to verification fragment, so they can enter their verficiation code.
+     *
      * @param credentials
      * @Author Emmett Kang
      * @Version 11 November 2018
      */
     @Override
-    public void onNotVerified(Credentials credentials) {
+    public void onNotVerified( Credentials credentials ) {
 
         //Tell the user that they're not verified.
-        Context context = getApplicationContext();
-        CharSequence text = "Your account isn't verified yet =[";
-        int duration = Toast.LENGTH_SHORT;
+        Context      context  = getApplicationContext();
+        CharSequence text     = "Your account isn't verified yet =[";
+        int          duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText( context, text, duration );
         toast.show();
 
 
         VerifyFragment verificationFragment = new VerifyFragment();
-        Bundle args = new Bundle();
+        Bundle         args                 = new Bundle();
         //Package the credentials to the next fragment.
-        args.putSerializable(getString(R.string.key_credential), credentials);
-        verificationFragment.setArguments(args);
+        args.putSerializable( getString( R.string.key_credential ), credentials );
+        verificationFragment.setArguments( args );
         FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frame_signin_container, verificationFragment)
-                .addToBackStack(null);
+                .replace( R.id.frame_signin_container, verificationFragment )
+                .addToBackStack( null );
         // Commit the transaction
         transaction.commit();
     }
@@ -105,12 +107,12 @@ public class SignInActivity extends AppCompatActivity
     @Override
     public void onRegisterClicked() {
         RegisterFragment userRegistrationFragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        userRegistrationFragment.setArguments(args);
+        Bundle           args                     = new Bundle();
+        userRegistrationFragment.setArguments( args );
         FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frame_signin_container, userRegistrationFragment)
-                .addToBackStack(null);
+                .replace( R.id.frame_signin_container, userRegistrationFragment )
+                .addToBackStack( null );
         // Commit the transaction
         transaction.commit();
     }
@@ -123,8 +125,8 @@ public class SignInActivity extends AppCompatActivity
     public void onWaitFragmentInteractionShow() {
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.frame_signin_container, new WaitFragment(), "WAIT")
-                .addToBackStack(null)
+                .add( R.id.frame_signin_container, new WaitFragment(), "WAIT" )
+                .addToBackStack( null )
                 .commitAllowingStateLoss();
     }
 
@@ -135,21 +137,21 @@ public class SignInActivity extends AppCompatActivity
     public void onWaitFragmentInteractionHide() {
         getSupportFragmentManager()
                 .beginTransaction()
-                .remove(getSupportFragmentManager().findFragmentByTag("WAIT"))
+                .remove( getSupportFragmentManager().findFragmentByTag( "WAIT" ) )
                 .commit();
     }
 
-    private void openMain(Credentials credentials) {
-        Intent intent = new Intent(this, HomeActivity.class);
+    private void openMain( Credentials credentials ) {
+        Intent intent = new Intent( this, HomeActivity.class );
         //Send credential to next activity
-        intent.putExtra(getString(R.string.key_credential), credentials);
+        intent.putExtra( getString( R.string.key_credential ), credentials );
 
         //Send whether if we're opening from notification.
-        intent.putExtra(getString(R.string.keys_intent_notification_msg), mLoadFromChatNotification);
-        if (mChatIDfromNotification != null) { //If from notification, send chatID to home activity.
-            intent.putExtra(getString(R.string.keys_intent_notification_chatID), Integer.parseInt(mChatIDfromNotification));
+        intent.putExtra( getString( R.string.keys_intent_notification_msg ), mLoadFromChatNotification );
+        if ( mChatIDfromNotification != null ) { //If from notification, send chatID to home activity.
+            intent.putExtra( getString( R.string.keys_intent_notification_chatID ), Integer.parseInt( mChatIDfromNotification ) );
         }
-        startActivity(intent);
+        startActivity( intent );
         //End this Activity and remove it from the Activity back stack.
         finish();
     }
@@ -157,29 +159,30 @@ public class SignInActivity extends AppCompatActivity
     /**
      * This method is called when registration part is done, and leads the
      * user to verification section.
+     *
      * @param c Credentials to pass onto verification fragment.
      * @Author Emmett Kang
      * @Version 11 November 2018
      */
     @Override
-    public void onRegisterSuccess(Credentials c) {
+    public void onRegisterSuccess( Credentials c ) {
 
         //Warning to the user on limited time to verify.
-        Context context = getApplicationContext();
-        CharSequence text = "You have 1 hour to verify your account!";
-        int duration = Toast.LENGTH_LONG;
+        Context      context  = getApplicationContext();
+        CharSequence text     = "You have 1 hour to verify your account!";
+        int          duration = Toast.LENGTH_LONG;
 
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText( context, text, duration );
         toast.show();
 
         Bundle args = new Bundle();
-        args.putSerializable(getString(R.string.key_credential), c); //Package the credential to next fragment.
+        args.putSerializable( getString( R.string.key_credential ), c ); //Package the credential to next fragment.
         VerifyFragment verificationFragment = new VerifyFragment();
-        verificationFragment.setArguments(args);
+        verificationFragment.setArguments( args );
         FragmentTransaction transaction = getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.frame_signin_container, verificationFragment)
-                .addToBackStack(null);
+                .replace( R.id.frame_signin_container, verificationFragment )
+                .addToBackStack( null );
         // Commit the transaction
         transaction.commit();
         //openMain(c);
@@ -187,12 +190,13 @@ public class SignInActivity extends AppCompatActivity
 
     /**
      * When verification is done, send the user to the weather(landing) Activity.
+     *
      * @Author Emmett Kang
      * @Version 11 November 2018
      */
     @Override
-    public void onVerificationSuccess(Credentials c) {
-        openMain(c);
+    public void onVerificationSuccess( Credentials c ) {
+        openMain( c );
     }
 
 
